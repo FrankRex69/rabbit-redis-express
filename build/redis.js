b285489e692d9ac98f2a4903c50385e210479c2b
FrankRex69 <francesco.re69@gmail.com>
2022-09-21 00:10:26 +0200

    feat: gitignore

--- build/redis.js
+++ build/redis.js
@@ -62,8 +62,3 @@ export const startShort = async (shortUrl) => {
         throw error;
     }
 };
-// const longUrl: string = "http://www.prova31.com"
-// const account: string = "fran4@gmail.com"
-// const shortUrlCheck = "http://v7qbw9"
-// console.log(await startLong(longUrl, account))
-// console.log(await startShort(shortUrlCheck))
